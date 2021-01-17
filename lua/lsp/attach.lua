f0d69a02c0db94816b595648496b98837bfd8f24
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-17 19:35:48 +1000

    Move lsp maps to on attach

--- lua/lsp/attach.lua
+++ lua/lsp/attach.lua
@@ -1,7 +1,23 @@
 local util = require('lspconfig').util
+local map = require("settings.utils").map
 
 -- function to attach completion when setting up lsp
 local on_attach = function(client)
+    map("n", 'K', "<Cmd>lua vim.lsp.buf.hover()<CR>")
+    map("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>")
+    map("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>")
+    map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+    map("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+    map("n", "gdt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+    map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+    map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+    map("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+    map("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+    map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+    map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+    map("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+    map("n", "]d", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
+
     if client.resolved_capabilities.document_formatting then
         util.nvim_multiline_command [[
             augroup lsp_format_on_save
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,5 +1,3 @@
-local map = require("settings.utils").map
-
 require("lsp.lsputils")
 require("lsp.diagnostics")
 require("lsp.treesitter")
@@ -7,19 +5,3 @@ require("lsp.treesitter")
 require("lsp.vim")
 require("lsp.rust")
 require("lsp.omnisharp")
-
--- LSP
-map("n", 'K', "<Cmd>lua vim.lsp.buf.hover()<CR>")
-map("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>")
-map("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>")
-map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-map("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-map("n", "gdt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-map("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-map("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
-map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
-map("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
-map("n", "]d", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
