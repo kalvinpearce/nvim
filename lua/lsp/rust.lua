a9b120ab301c1cc1a884c85f8ec6bb2682a2216e
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-18 23:15:57 +1000

    Fix rust attach

--- lua/lsp/rust.lua
+++ lua/lsp/rust.lua
@@ -1,5 +1,5 @@
 local lspconfig = require('lspconfig')
-local on_attach = require("lsp.attach").on_attach
+local on_attach = require("lsp.attach").attach
 
 lspconfig.rust_analyzer.setup {
   on_attach = on_attach
