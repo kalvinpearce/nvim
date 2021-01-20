38ffa43ccdea2b5a6bbdda1ad662ac172b82f684
Kalvin Pearce <kalvinpearce@gmail.com>
2021-01-20 22:32:28 +1000

    Fix quickscope highlighting

--- lua/plugins/quickscope.lua
+++ lua/plugins/quickscope.lua
@@ -1,6 +1,13 @@
 local g, cmd = vim.g, vim.cmd
+local exec = vim.api.nvim_exec
 
 g.qs_highlight_on_keys = { "f", "F", "t", "T" }
 g.qs_max_chars = 150
-cmd "highlight QuickScopePrimary guifg='#00C7DF' gui=underline ctermfg=155 cterm=underline"
-cmd "highlight QuickScopeSecondary guifg='#afff5f' gui=underline ctermfg=81 cterm=underline"
+g.qs_hi_priority = 10000
+exec([[
+    augroup qs_colors
+        autocmd!
+        autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+        autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+    augroup END
+]],"")
