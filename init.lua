11323b8fb2e1dedc0173d8243f3d19bc0a048995
Kalvin Pearce <kalvinpearce@gmail.com>
2021-02-22 22:07:51 +1000

    Add default repos path to startup

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ require("settings")
 require("plugins")
 require("coc")
 
+vim.cmd "cd d:/Repos/"
+
 function Source()
     require('plenary.reload').reload_module('settings')
     require('plenary.reload').reload_module('plugins')
