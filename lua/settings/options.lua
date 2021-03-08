40943270338cbb50e3038d88220c56a3cf198845
Kalvin Pearce <kalvinpearce@gmail.com>
2021-03-08 23:59:47 +1000

    Refactor options due to lua buffer thing

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -1,51 +1,49 @@
 local cmd = vim.cmd
 local fn = vim.fn
-local apply_options = require("settings.utils").apply_options
+local g = vim.g
 
-cmd "filetype plugin indent on"
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
-apply_options({
-    autoindent = true, -- enable autoindent
-    backup = false, -- disable backup
-    cursorline = true, -- enable cursorline
-    expandtab = true, -- use spaces instead of tabs
-    hidden = true, -- keep hidden buffers
-    ignorecase = true, -- case insensitive on search
-    lazyredraw = true, -- lazyredraw to make macro faster
-    number = true, -- enable number
-    relativenumber = true, -- enable relativenumber
-    showmode = false, -- don't show mode
-    smartcase = true, -- improve searching using '/'
-    smartindent = true, -- smarter indentation
-    splitbelow = true, -- split below instead of above
-    splitright = true, -- split right instead of left
-    swapfile = false, -- disable swapfile
-    termguicolors = true, -- truecolours for better experience
-    wrap = false, -- dont wrap lines
-    writebackup = false, -- disable backup
+local function opt(scope, key, value)
+  scopes[scope][key] = value
+  if scope ~= 'o' then scopes['o'][key] = value end
+end
 
-    clipboard = "unnamedplus", -- use system clipboard
-    completeopt = "menuone,noinsert", -- better completion
-    encoding = "UTF-8", -- set encoding
-    foldmethod = "marker", -- foldmethod using marker
-    formatoptions = "qrn1", -- improve editor formatting
-    mouse = "a",
-    pastetoggle="<F10>",
-    shortmess = "filnxtToOFIWc", -- disable some stuff on shortmess
-    signcolumn = "yes", -- enable sign column all the time, 4 column
-    syntax = "on", -- syntax enable
+opt('o', 'backup', false) -- disable backup
+opt('w', 'cursorline', true) -- enable cursorline
+opt('o', 'expandtab', true) -- use spaces instead of tabs
+opt('o', 'hidden', true) -- keep hidden buffers
+opt('o', 'ignorecase', true) -- case insensitive on search
+opt('o', 'lazyredraw', true) -- lazyredraw to make macro faster
+opt('w', 'number', true) -- enable number
+opt('w', 'relativenumber', true) -- enable relativenumber
+opt('o', 'showmode', false) -- don't show mode
+opt('o', 'smartcase', true) -- improve searching using '/'
+opt('b', 'smartindent', true) -- smarter indentation
+opt('o', 'splitbelow', true) -- split below instead of above
+opt('o', 'splitright', true) -- split right instead of left
+opt('b', 'swapfile', false) -- disable swapfile
+opt('o', 'termguicolors', true) -- truecolours for better experience
+opt('w', 'wrap', false) -- dont wrap lines
+opt('o', 'writebackup', false) -- disable backup
 
-    cmdheight = 2,
-    colorcolumn = 80, -- 120 chars color column
-    conceallevel = 0,
-    laststatus = 2, -- always enable statusline
-    pumheight = 20, -- limit completion items
-    shiftwidth = 4, -- set indentation width
-    synmaxcol = 300, -- set limit for syntax highlighting in a single line
-    timeoutlen = 400, -- faster timeout wait time
-    updatetime = 100, -- set faster update time
+opt('o', 'clipboard', 'unnamedplus') -- use system clipboard
+opt('w', 'colorcolumn', '80') -- 80 chars color column (angry this is string)
+opt('o', 'completeopt', 'menuone,noinsert') -- better completion
+opt('b', 'formatoptions', 'qrn1') -- improve editor formatting
+opt('o', 'guifont', 'Hack Nerd Font') -- set nerd font
+opt('o', 'mouse', 'a')
+opt('o', 'pastetoggle', '<F10>')
+opt('o', 'shortmess', 'filnxtToOFIWc') -- disable some stuff on shortmess
+opt('w', 'signcolumn', 'yes') -- enable sign column all the time, 4 column
+opt('b', 'syntax', 'on') -- syntax enable
 
-})
+opt('o', 'cmdheight', 2)
+opt('w', 'conceallevel', 0)
+opt('o', 'pumheight', 20) -- limit completion items
+opt('b', 'shiftwidth', 4) -- set indentation width
+opt('o', 'timeoutlen', 400) -- faster timeout wait time
+opt('o', 'updatetime', 100) -- set faster update time
 
 if fn.has("win32") == 1 then
     cmd [[ let &shell = "powershell" ]]
@@ -54,4 +52,24 @@ if fn.has("win32") == 1 then
     cmd [[ set shellredir=\|\ Out-File\ -Encoding\ UTF8 ]]
 end
 
-cmd [[ set guifont=Hack\ Nerd\ Font ]]
+-- Provider skips
+g.loaded_ruby_provider = 0
+g.loaded_perl_provider = 0
+-- Plugin skips
+g.loaded_gzip = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_matchit = 0
+g.loaded_matchparen = 0
+g.loaded_2html_plugin = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+g.loaded_netrwPlugin = 1
+g.loaded_netrwSettings = 1
+g.loaded_netrwFileHandlers = 1
--- lua/settings/utils.lua
+++ lua/settings/utils.lua
@@ -1,5 +1,4 @@
 local fn = vim.fn
-local cmd = vim.cmd
 local tbl_extend = vim.tbl_extend
 local U = {}
 
@@ -14,18 +13,6 @@ function U.map(mode, key, result, opts)
         }
     )
     fn.nvim_set_keymap(mode, key, result, opts)
-end
-
-function U.apply_options(opts)
-    for k, v in pairs(opts) do
-        if v == true then
-            cmd("set " .. k)
-        elseif v == false then
-            cmd(string.format("set no%s", k))
-        else
-            cmd(string.format("set %s=%s", k, v))
-        end
-    end
 end
 
 return U
