93b3789c28f2a661511f58a15fba767cfcc187eb
Kalvin Pearce <kalvinpearce@gmail.com>
2021-10-19 19:27:09 +1000

    Update to use vim.opt
    Fixes #2

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -1,51 +1,46 @@
 local cmd = vim.cmd
 local fn = vim.fn
 local g = vim.g
+local opt = vim.opt
 
-local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+opt.backup = false -- disable backup
+opt.cursorline = true -- enable cursorline
+opt.expandtab = true -- use spaces instead of tabs
+opt.hidden = true -- keep hidden buffers
+opt.ignorecase = true -- case insensitive on search
+opt.lazyredraw = true -- lazyredraw to make macro faster
+opt.number = true -- enable number
+opt.relativenumber = true -- enable relativenumber
+opt.showmode = false -- don't show mode
+opt.smartcase = true -- improve searching using '/'
+opt.smartindent = true -- smarter indentation
+opt.splitbelow = true -- split below instead of above
+opt.splitright = true -- split right instead of left
+opt.swapfile = false -- disable swapfile
+opt.termguicolors = true -- truecolours for better experience
+opt.wrap = false -- dont wrap lines
+opt.writebackup = false -- disable backup
 
-local function opt(scope, key, value)
-  scopes[scope][key] = value
-  if scope ~= 'o' then scopes['o'][key] = value end
-end
+opt.clipboard = 'unnamedplus' -- use system clipboard
+opt.colorcolumn = '80' -- 80 chars color column (angry this is string)
+opt.completeopt = { 'menuone,noinsert' } -- better completion
+opt.formatoptions = 'qrn1' -- improve editor formatting
+opt.guifont = 'Hack Nerd Font' -- set nerd font
+opt.mouse = 'a'
+opt.pastetoggle = '<F10>'
+opt.shortmess = 'filnxtToOFIWc' -- disable some stuff on shortmess
+opt.signcolumn = 'yes' -- enable sign column all the time, 4 column
+opt.syntax = 'on' -- syntax enable
 
-opt('o', 'backup', false) -- disable backup
-opt('w', 'cursorline', true) -- enable cursorline
-opt('o', 'expandtab', true) -- use spaces instead of tabs
-opt('o', 'hidden', true) -- keep hidden buffers
-opt('o', 'ignorecase', true) -- case insensitive on search
-opt('o', 'lazyredraw', true) -- lazyredraw to make macro faster
-opt('w', 'number', true) -- enable number
-opt('w', 'relativenumber', true) -- enable relativenumber
-opt('o', 'showmode', false) -- don't show mode
-opt('o', 'smartcase', true) -- improve searching using '/'
-opt('b', 'smartindent', true) -- smarter indentation
-opt('o', 'splitbelow', true) -- split below instead of above
-opt('o', 'splitright', true) -- split right instead of left
-opt('b', 'swapfile', false) -- disable swapfile
-opt('o', 'termguicolors', true) -- truecolours for better experience
-opt('w', 'wrap', false) -- dont wrap lines
-opt('o', 'writebackup', false) -- disable backup
-
-opt('o', 'clipboard', 'unnamedplus') -- use system clipboard
-opt('w', 'colorcolumn', '80') -- 80 chars color column (angry this is string)
-opt('o', 'completeopt', 'menuone,noinsert') -- better completion
-opt('b', 'formatoptions', 'qrn1') -- improve editor formatting
-opt('o', 'guifont', 'Hack Nerd Font') -- set nerd font
-opt('o', 'mouse', 'a')
-opt('o', 'pastetoggle', '<F10>')
-opt('o', 'shortmess', 'filnxtToOFIWc') -- disable some stuff on shortmess
-opt('w', 'signcolumn', 'yes') -- enable sign column all the time, 4 column
-opt('b', 'syntax', 'on') -- syntax enable
-
-opt('o', 'cmdheight', 2)
-opt('w', 'conceallevel', 0)
-opt('o', 'pumheight', 20) -- limit completion items
-opt('b', 'shiftwidth', 4) -- set indentation width
-opt('o', 'timeoutlen', 400) -- faster timeout wait time
-opt('o', 'updatetime', 100) -- set faster update time
+opt.cmdheight = 2
+opt.conceallevel = 0
+opt.pumheight = 20 -- limit completion items
+opt.shiftwidth = 4 -- set indentation width
+opt.timeoutlen = 400 -- faster timeout wait time
+opt.updatetime = 100 -- set faster update time
 
 if fn.has("win32") == 1 then
+    -- Set shell to powershell
     cmd [[ let &shell = "powershell" ]]
     cmd [[ set shellquote= shellpipe=\| shellxquote= ]]
     cmd [[ set shellcmdflag=-NoLogo\ -NoProfile\ -ExecutionPolicy\ RemoteSigned\ -Command ]]
