2e2e52252df1fd68bd5cb08e913495392868d4c0
Kalvin Pearce <kalvinpearce@gmail.com>
2021-11-09 18:53:50 +1000

    Fix treesitter win32 support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,7 @@
+if vim.fn.has("win32") == 1 then
+  require 'nvim-treesitter.install'.compilers = { "clang" }
+end
+
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained",
   highlight = {
