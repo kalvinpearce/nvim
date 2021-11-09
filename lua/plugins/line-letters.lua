989168dbb0958822d11ffe0f6df0a6f7192e2ab4
Kalvin Pearce <kalvinpearce@gmail.com>
2021-11-09 21:27:24 +1000

    Remove old plugin configs

--- lua/plugins/line-letters.lua
+++
@@ -1,3 +0,0 @@
-local map = require("settings.utils").map
-
-map("n", "|", "<Plug>LineLetters", {silent = true, noremap = false})
--- lua/plugins/test.lua
+++
@@ -1,3 +0,0 @@
-local map = require("settings.utils").map
-
-map("n", "<leader>tf", "<cmd>TestFile<CR>", {})
