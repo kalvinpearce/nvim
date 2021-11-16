52f485027e1c18c01ed51696092a4bbba58d16fd
Kalvin Pearce <kalvinpearce@gmail.com>
2021-11-16 21:52:43 +1000

    Remove smooth scroller

--- lua/settings/plugins.lua
+++ lua/settings/plugins.lua
@@ -45,7 +45,6 @@ return require("packer").startup(
       use "rrethy/vim-illuminate" -- highlight matching words when cursor on it
       use "romainl/vim-cool" -- kill highlight when moving off searched word
       use "machakann/vim-highlightedyank"
-      use "joeytwiddle/sexy_scroller.vim"
 
       -- Manual Tweeks
       use { 'numToStr/Comment.nvim',
