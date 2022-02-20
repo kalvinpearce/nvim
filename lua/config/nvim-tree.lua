f7e0acdc0db70a299eaf2516409e68bb4d6e6270
Kalvin Pearce <kalvinpearce@gmail.com>
2022-02-20 18:02:57 +1000

    Remove unused nvim-tree variables

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -27,13 +27,6 @@ local function config()
 		return
 	end
 
-	local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-	if not config_status_ok then
-		return
-	end
-
-	local tree_cb = nvim_tree_config.nvim_tree_callback
-
 	nvim_tree.setup({
 		disable_netrw = true,
 		hijack_netrw = true,
