2a92fcd298134b44b46964fa4ca98f17f328f4d1
Kalvin Pearce <kalvinpearce@gmail.com>
2022-02-27 14:10:51 +1000

    Fix outdated diagnostic call

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -72,7 +72,7 @@ local function lsp_keymaps(bufnr)
 		bufnr,
 		"n",
 		"gl",
-		'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
+		'<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>',
 		opts
 	)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
