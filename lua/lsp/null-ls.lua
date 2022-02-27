ac1106cb44a56c463bf824b80f9952496e0e7809
Kalvin Pearce <kalvinpearce@gmail.com>
2022-02-27 15:13:46 +1000

    Add null-ls eslint

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -4,13 +4,16 @@ if not null_ls_status_ok then
 end
 
 local formatting = null_ls.builtins.formatting
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier,
 		formatting.stylua,
+    diagnostics.eslint,
+    code_actions.eslint,
 	},
 	-- on_attach = function(client)
 	-- 	if client.resolved_capabilities.document_formatting then
