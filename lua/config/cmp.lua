507ce94af60349e96908b2ca14d1a833d7b26517
Kalvin Pearce <kalvinpearce@gmail.com>
2022-03-14 01:00:40 +1000

    Fix misc cmp binds

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -67,13 +67,21 @@ local function config()
 			-- Accept currently selected item. If none selected, `select` first item.
 			-- Set `select` to `false` to only confirm explicitly selected items.
 			["<CR>"] = cmp.mapping.confirm({ select = true }),
+			-- ["<ESC>"] = cmp.mapping(function(fallback)
+			-- 	if cmp.visible() then
+			-- 		cmp.close()
+			-- 	else
+			-- 		fallback()
+			-- 	end
+			-- end, {
+			-- 	"i",
+			-- 	"s",
+			-- }),
 			["<Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
-				elseif luasnip.expandable() then
-					luasnip.expand()
-				elseif luasnip.expand_or_locally_jumpable() then
-					luasnip.expand_or_jump()
+				elseif luasnip.in_snippet() and luasnip.jumpable() then
+					luasnip.jump(1)
 				elseif check_backspace() then
 					fallback()
 				else
