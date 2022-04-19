2b0a844d6d48662a42acadd79eb3b03a8e84e37d
Kalvin Pearce <kalvinpearce@gmail.com>
2022-04-20 00:03:06 +1000

    Fix lua formatting prompt

--- lua/lsp/settings/sumneko_lua.lua
+++ lua/lsp/settings/sumneko_lua.lua
@@ -1,14 +1,24 @@
 return {
 	settings = {
 		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
 			diagnostics = {
 				globals = { "vim", "jit" },
 			},
+			format = {
+				enable = false,
+			},
 			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
+				library = vim.api.nvim_get_runtime_file("", true),
+				-- library = {
+				-- 	[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+				-- 	[vim.fn.stdpath("config") .. "/lua"] = true,
+				-- },
+			},
+			telemetry = {
+				enable = false,
 			},
 		},
 	},
