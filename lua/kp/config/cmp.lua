a538008913a702bd4372aed99244f9dcba8e1689
Kalvin Pearce <kalvinpearce@gmail.com>
2022-04-25 23:57:29 +1000

    Clean up cmp

--- lua/kp/config/cmp.lua
+++ lua/kp/config/cmp.lua
@@ -59,26 +59,14 @@ local function config()
 			["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 			["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 			["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c", "n" }),
+			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c", "n" }),
 			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 			["<C-e>"] = cmp.mapping({
 				i = cmp.mapping.abort(),
 				c = cmp.mapping.close(),
 			}),
-			-- Accept currently selected item. If none selected, `select` first item.
-			-- Set `select` to `false` to only confirm explicitly selected items.
 			["<CR>"] = cmp.mapping.confirm({ select = true }),
-			-- ["<ESC>"] = cmp.mapping(function(fallback)
-			-- 	if cmp.visible() then
-			-- 		cmp.close()
-			-- 	else
-			-- 		fallback()
-			-- 	end
-			-- end, {
-			-- 	"i",
-			-- 	"s",
-			-- }),
 			["<Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
@@ -142,14 +130,6 @@ local function config()
 				compare.kind,
 				-- compare.length, -- useless
 			},
-		},
-		window = {
-			documentation = {
-				border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-			},
-		},
-		experimental = {
-			ghost_text = false,
 		},
 	})
 end
