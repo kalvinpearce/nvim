abfeeaf7b86e4ed3ceb5e44f0f28bb986427179c
Kalvin Pearce <kalvinpearce@gmail.com>
2022-06-19 13:49:21 +1000

    Improve lua function snippets

--- lua/kp/lsp/settings/sumneko_lua.lua
+++ lua/kp/lsp/settings/sumneko_lua.lua
@@ -10,12 +10,16 @@ return {
 			format = {
 				enable = false,
 			},
+			completion = {
+				keywordSnippet = "Replace",
+				callSnippet = "Replace",
+			},
 			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				-- library = {
-				-- 	[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-				-- 	[vim.fn.stdpath("config") .. "/lua"] = true,
-				-- },
+				-- library = vim.api.nvim_get_runtime_file("", true),
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
 			},
 			telemetry = {
 				enable = false,
