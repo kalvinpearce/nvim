50e282490bce8060ea64e3d92a2d2b5474214caa
Kalvin Pearce <kalvinpearce@gmail.com>
2022-07-10 15:41:41 +1000

    Fix dial new config

--- lua/kp/config/dial-nvim.lua
+++ lua/kp/config/dial-nvim.lua
@@ -1,29 +1,27 @@
 local function config()
-	local status_ok, dial = pcall(require, "dial")
+	local status_ok, dialConfig = pcall(require, "dial.config")
 	if not status_ok then
 		return
 	end
 
-	local map = require("kp.utils").map
-	map("n", "<C-a>", "<Plug>(dial-increment)", { noremap = false })
-	map("n", "<C-x>", "<Plug>(dial-decrement)", { noremap = false })
-	map("v", "<C-a>", "<Plug>(dial-increment)", { noremap = false })
-	map("v", "<C-x>", "<Plug>(dial-decrement)", { noremap = false })
-	map("v", "g<C-a>", "<Plug>(dial-increment-additional)", { noremap = false })
-	map("v", "g<C-x>", "<Plug>(dial-decrement-additional)", { noremap = false })
+	local augend = require("dial.augend")
+	dialConfig.augends:register_group({
+		default = {
+			augend.constant.alias.bool,
+			augend.integer.alias.decimal,
+			augend.integer.alias.hex,
+			augend.date.alias["%Y/%m/%d"],
+		},
+	})
 
-  dial.augends["custom#boolean"] = dial.common.enum_cyclic{
-    name = "boolean",
-    strlist = {"true", "false"},
-  }
-  dial.config.searchlist.normal = {
-    "number#decimal",
-    "number#hex",
-    "number#binary",
-    "date#[%Y/%m/%d]",
-    "markup#markdown#header",
-    "custom#boolean",
-  }
+	local map = require("kp.utils").map
+	local dialMap = require("dial.map")
+	map("n", "<C-a>", dialMap.inc_normal(), { noremap = true })
+	map("n", "<C-x>", dialMap.dec_normal(), { noremap = true })
+	map("v", "<C-a>", dialMap.inc_visual(), { noremap = true })
+	map("v", "<C-x>", dialMap.dec_visual(), { noremap = true })
+	map("v", "g<C-a>", dialMap.inc_gvisual(), { noremap = true })
+	map("v", "g<C-x>", dialMap.dec_gvisual(), { noremap = true })
 end
 
 local function init(use)
