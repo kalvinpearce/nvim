0118bba2459454bedcbe5b15ffa173888debb5a6
Kalvin Pearce <kalvinpearce@gmail.com>
2022-10-02 21:59:20 +1000

    Fix lsp errors

--- lua/kp/lsp/handlers.lua
+++ lua/kp/lsp/handlers.lua
@@ -41,7 +41,7 @@ end
 
 local function lsp_highlight_document(client)
 	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
+	if client.server_capabilities.document_highlight then
 		vim.api.nvim_exec(
 			[[
       augroup lsp_document_highlight
@@ -79,9 +79,9 @@ end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	elseif client.name == "sumneko_lua" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
--- lua/kp/lsp/null-ls.lua
+++ lua/kp/lsp/null-ls.lua
@@ -17,7 +17,7 @@ null_ls.setup({
 		formatting.rustywind,
 	},
 	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
+		if client.server_capabilities.document_formatting then
 			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
 		end
 	end,
