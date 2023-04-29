edc48ed06bd848149f58d0c0616d615ac7daa21c
Kalvin Pearce <kalvinpearce@gmail.com>
2023-04-29 12:02:13 +1000

    Add better word nav

--- lua/kp/plugins/editor.lua
+++ lua/kp/plugins/editor.lua
@@ -334,6 +334,17 @@ return {
   },
 
   {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<cr>", mode = { "n", "o", "x" } },
+      { "e", "<cmd>lua require('spider').motion('e')<cr>", mode = { "n", "o", "x" } },
+      { "b", "<cmd>lua require('spider').motion('b')<cr>", mode = { "n", "o", "x" } },
+      { "ge", "<cmd>lua require('spider').motion('ge')<cr>", mode = { "n", "o", "x" } },
+    },
+  },
+
+  {
     "sindrets/diffview.nvim",
   },
 
