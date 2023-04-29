f50b9b4fe9ba30c5613a600a685ac2dc4ea8e864
Kalvin Pearce <kalvinpearce@gmail.com>
2023-04-29 11:55:57 +1000

    Swap to catppuccin theme

--- lua/kp/plugins/colorscheme.lua
+++ lua/kp/plugins/colorscheme.lua
@@ -2,15 +2,23 @@ return {
   {
     "uloco/bluloco.nvim",
     dependencies = { "rktjmp/lush.nvim" },
-    lazy = false,
-    priority = 1000,
+    lazy = true,
     config = function()
-      vim.cmd([[colorscheme bluloco]])
+      -- vim.cmd([[colorscheme bluloco]])
     end,
   },
   {
     "folke/tokyonight.nvim",
     opts = { style = "storm" },
     lazy = true,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin-frappe]])
+    end,
   },
 }
