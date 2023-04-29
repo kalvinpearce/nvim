ea6dc49c516f612b1a8f82e0753680e22b8afcb6
Kalvin Pearce <kalvinpearce@gmail.com>
2023-04-29 12:01:05 +1000

    Use only git matching for directory changing

--- lua/kp/plugins/editor.lua
+++ lua/kp/plugins/editor.lua
@@ -675,8 +675,8 @@ return {
     "ahmedkhalf/project.nvim",
     config = function()
       require("project_nvim").setup({
-        detection_methods = { "pattern", "lsp" },
-        patterns = { ".git", "package.json", "cargo.toml" },
+        detection_methods = { "pattern" },
+        patterns = { ".git" },
       })
       require("telescope").load_extension("projects")
     end,
