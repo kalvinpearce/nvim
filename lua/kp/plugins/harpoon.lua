4c9a34eeb3a2a1a389c5f94be4b54293eda931ab
Kalvin Pearce <kalvinpearce@gmail.com>
2024-01-27 16:36:32 +1000

    fix: adjust harpoon mappings

--- lua/kp/plugins/harpoon.lua
+++ lua/kp/plugins/harpoon.lua
@@ -3,57 +3,18 @@ return {
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    keys = {
-      {
-        "<a-n>",
-        function()
-          require("harpoon.mark").add_file()
-        end,
-        desc = "Add file",
-      },
-      {
-        "<a-h>",
-        function()
-          require("harpoon.ui").toggle_quick_menu()
-        end,
-        desc = "Quick menu",
-      },
-      {
-        "<a-u>",
-        function()
-          require("harpoon.cmd-ui").toggle_quick_menu()
-        end,
-        desc = "Quick menu",
-      },
-      {
-        "<a-j>",
-        function()
-          require("harpoon.ui").nav_file(1)
-        end,
-        desc = "Nav file 1",
-      },
-      {
-        "<a-k>",
-        function()
-          require("harpoon.ui").nav_file(2)
-        end,
-        desc = "Nav file 2",
-      },
-      {
-        "<a-l>",
-        function()
-          require("harpoon.ui").nav_file(3)
-        end,
-        desc = "Nav file 3",
-      },
-      {
-        "<a-;>",
-        function()
-          require("harpoon.ui").nav_file(4)
-        end,
-        desc = "Nav file 4",
-      },
-    },
-    config = true,
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+
+      vim.keymap.set("n", "<leader>hg", function() harpoon:list():append() end, { desc = "Add file" })
+      vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+        { desc = "Quick menu" })
+
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():select(1) end, { desc = "Nav file 1" })
+      vim.keymap.set("n", "<leader>hs", function() harpoon:list():select(2) end, { desc = "Nav file 2" })
+      vim.keymap.set("n", "<leader>hd", function() harpoon:list():select(3) end, { desc = "Nav file 3" })
+      vim.keymap.set("n", "<leader>hf", function() harpoon:list():select(4) end, { desc = "Nav file 4" })
+    end,
   },
 }
