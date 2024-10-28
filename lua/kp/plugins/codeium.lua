e9b1d16fa4c3d276d8fb212411793db34cf34dde
Kalvin Pearce <kalvinpearce@gmail.com>
2024-10-28 21:34:56 +1000

    feat: lazyload codeium

--- lua/kp/plugins/codeium.lua
+++ lua/kp/plugins/codeium.lua
@@ -1,22 +1,43 @@
 return {
   {
     "Exafunction/codeium.vim",
-    event = "BufEnter",
-    config = function()
+    keys = {
+      {
+        mode = "i",
+        "<M-left>",
+        function()
+          return vim.fn["codeium#Complete"]()
+        end,
+        desc = "Codeium Trigger Suggestion",
+      },
+      {
+        mode = "i",
+        "<M-right>",
+        function()
+          return vim.fn["codeium#Accept"]()
+        end,
+        desc = "Codeium Accept Suggestion",
+      },
+      {
+        mode = "i",
+        "<M-down>",
+        function()
+          return vim.fn["codeium#CycleCompletions"](1)
+        end,
+        desc = "Codeium Cycle Suggestions",
+      },
+      {
+        mode = "i",
+        "<M-up>",
+        function()
+          return vim.fn["codeium#CycleCompletions"](-1)
+        end,
+        desc = "Codeium Cycle Suggestions",
+      },
+    },
+    init = function()
       vim.g.codeium_disable_bindings = 1
       vim.g.codeium_manual = true
-      vim.keymap.set("i", "<M-right>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true, silent = true })
-      vim.keymap.set("i", "<M-down>", function()
-        return vim.fn["codeium#CycleCompletions"](1)
-      end, { expr = true, silent = true })
-      vim.keymap.set("i", "<M-up>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, { expr = true, silent = true })
-      vim.keymap.set("i", "<M-left>", function()
-        return vim.fn["codeium#Complete"]()
-      end, { expr = true, silent = true })
     end,
   },
 }
