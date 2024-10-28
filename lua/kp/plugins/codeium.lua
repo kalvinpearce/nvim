501d0beb87178ef02c29e605cd0b5de7c0a3b2fa
Kalvin Pearce <kalvinpearce@gmail.com>
2024-10-28 22:11:48 +1000

    fix: codeium not accepting

--- lua/kp/plugins/codeium.lua
+++ lua/kp/plugins/codeium.lua
@@ -2,42 +2,28 @@ return {
   {
     "Exafunction/codeium.vim",
     keys = {
-      {
-        mode = "i",
-        "<M-left>",
-        function()
-          return vim.fn["codeium#Complete"]()
-        end,
-        desc = "Codeium Trigger Suggestion",
-      },
-      {
-        mode = "i",
-        "<M-right>",
-        function()
-          return vim.fn["codeium#Accept"]()
-        end,
-        desc = "Codeium Accept Suggestion",
-      },
-      {
-        mode = "i",
-        "<M-down>",
-        function()
-          return vim.fn["codeium#CycleCompletions"](1)
-        end,
-        desc = "Codeium Cycle Suggestions",
-      },
-      {
-        mode = "i",
-        "<M-up>",
-        function()
-          return vim.fn["codeium#CycleCompletions"](-1)
-        end,
-        desc = "Codeium Cycle Suggestions",
-      },
+      { mode = "i", "<M-left>", desc = "Codeium Trigger Suggestion" },
+      { mode = "i", "<M-right>", desc = "Codeium Accept Suggestion" },
+      { mode = "i", "<M-down>", desc = "Codeium Cycle Suggestions" },
+      { mode = "i", "<M-up>", desc = "Codeium Cycle Suggestions" },
     },
     init = function()
       vim.g.codeium_disable_bindings = 1
       vim.g.codeium_manual = true
+    end,
+    config = function()
+      vim.keymap.set("i", "<M-left>", function()
+        return vim.fn["codeium#Complete"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<M-right>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<M-down>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<M-up>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
     end,
   },
 }
