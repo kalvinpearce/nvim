f57d2d868c69a8d455ad7f1e452b754a123cba91
Kalvin Pearce <kalvinpearce@gmail.com>
2024-10-28 23:28:34 +1000

    feat: lazyload markdown-preview

--- lua/kp/plugins/misc.lua
+++ lua/kp/plugins/misc.lua
@@ -81,9 +81,27 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
     build = function()
       vim.fn["mkdp#util#install"]()
+    end,
+    init = function()
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_command_for_global = 1
+      vim.g.mkdp_combine_preview = 1
+
+      ---Fixes "No command :MarkdownPreview"
+      ---https://github.com/iamcco/markdown-preview.nvim/issues/585#issuecomment-1724859362
+      local function load_then_exec(cmd)
+        return function()
+          vim.cmd.delcommand(cmd)
+          require("lazy").load { plugins = { "markdown-preview.nvim" } }
+          vim.api.nvim_exec_autocmds("BufEnter", {}) -- commands appear only after BufEnter
+          vim.cmd(cmd)
+        end
+      end
+      for _, cmd in pairs { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" } do
+        vim.api.nvim_create_user_command(cmd, load_then_exec(cmd), {})
+      end
     end,
   },
 
