d9b969c1507173fb70afb84c4e88f6ee9dd93830
Kalvin Pearce <kalvinpearce@gmail.com>
2024-10-28 22:52:36 +1000

    feat: lazyload flash & remove conflicted key

--- lua/kp/plugins/navigation.lua
+++ lua/kp/plugins/navigation.lua
@@ -2,7 +2,6 @@ return {
   -- Navigate your code with search labels, enhanced character motions, and Treesitter
   {
     "folke/flash.nvim",
-    event = "VeryLazy",
     ---@type Flash.Config
     opts = {},
     -- stylua: ignore
@@ -11,7 +10,6 @@ return {
       { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
 
