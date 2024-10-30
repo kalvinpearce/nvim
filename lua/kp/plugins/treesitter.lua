efef5f2d95d6a2c2ae9f359bb7285e9119f3b7bb
Kalvin Pearce <kalvinpearce@gmail.com>
2024-10-30 22:12:47 +1000

    feat: simplify treesitter

--- lua/kp/plugins/treesitter.lua
+++ lua/kp/plugins/treesitter.lua
@@ -1,4 +1,3 @@
-local load_textobjects = false
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -7,16 +6,7 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
       "windwp/nvim-ts-autotag",
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        init = function()
-          -- disable rtp plugin, as we only need its queries for mini.ai
-          -- In case other textobject modules are enabled, we will load them
-          -- once nvim-treesitter is loaded
-          require("lazy.core.loader").disable_rtp_plugin "nvim-treesitter-textobjects"
-          load_textobjects = true
-        end,
-      },
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
     cmd = { "TSUpdateSync" },
     keys = {
@@ -60,35 +50,8 @@ return {
         enable = true,
       },
     },
-    ---@param opts TSConfig
     config = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        ---@type table<string, boolean>
-        local added = {}
-        opts.ensure_installed = vim.tbl_filter(function(lang)
-          if added[lang] then
-            return false
-          end
-          added[lang] = true
-          return true
-        end, opts.ensure_installed)
-      end
       require("nvim-treesitter.configs").setup(opts)
-
-      if load_textobjects then
-        -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-        if opts.textobjects then
-          for _, mod in ipairs { "move", "select", "swap", "lsp_interop" } do
-            if opts.textobjects[mod] and opts.textobjects[mod].enable then
-              local Loader = require "lazy.core.loader"
-              Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
-              local plugin = require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
-              require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
-              break
-            end
-          end
-        end
-      end
     end,
   },
 }
