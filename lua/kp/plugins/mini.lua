329b7e47210021d24ea5766b99feb373a9fb0ec7
Kalvin Pearce <kalvinpearce@gmail.com>
2025-01-31 14:38:17 +1000

    fix: lazy init event

--- lua/kp/plugins/mini.lua
+++ lua/kp/plugins/mini.lua
@@ -1,7 +1,8 @@
 return {
   {
     "echasnovski/mini.nvim",
-    event = "VimEnter",
+    priority = 1000,
+    lazy = false,
     name = "mini",
     version = false,
     init = function()
@@ -114,12 +115,12 @@ return {
       },
       surround = {
         mappings = {
-          add = "gza", -- Add surrounding in Normal and Visual modes
-          delete = "gzd", -- Delete surrounding
-          find = "gzf", -- Find surrounding (to the right)
-          find_left = "gzF", -- Find surrounding (to the left)
-          highlight = "gzh", -- Highlight surrounding
-          replace = "gzr", -- Replace surrounding
+          add = "gza",            -- Add surrounding in Normal and Visual modes
+          delete = "gzd",         -- Delete surrounding
+          find = "gzf",           -- Find surrounding (to the right)
+          find_left = "gzF",      -- Find surrounding (to the left)
+          highlight = "gzh",      -- Highlight surrounding
+          replace = "gzr",        -- Replace surrounding
           update_n_lines = "gzn", -- Update `n_lines`
         },
       },
