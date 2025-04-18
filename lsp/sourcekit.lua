946e050883e9bd5a4c8953fde10ac2992837e01d
Kalvin Pearce <kalvinpearce@gmail.com>
2025-04-18 14:03:36 +1000

    feat: add missing lsps

---
+++ lsp/sourcekit.lua
@@ -0,0 +1,28 @@
+return {
+  cmd = { 'sourcekit-lsp' },
+  filetypes = { 'swift', 'objc', 'objcpp', 'c', 'cpp' },
+  root_markers = {
+    "buildServer.json",
+    "*.xcodeproj",
+    "*.xcworkspace",
+    "compile_commands.json",
+    "Package.swift",
+  },
+  get_language_id = function(_, ftype)
+    local t = { objc = 'objective-c', objcpp = 'objective-cpp' }
+    return t[ftype] or ftype
+  end,
+  capabilities = {
+    workspace = {
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+      },
+    },
+    textDocument = {
+      diagnostic = {
+        dynamicRegistration = true,
+        relatedDocumentSupport = true,
+      },
+    },
+  },
+}
---
+++ lsp/wgsl_analyzer.lua
@@ -0,0 +1,6 @@
+return {
+  cmd = { 'wgsl-analyzer' },
+  filetypes = { 'wgsl' },
+  root_markers = { '.git' },
+  settings = {},
+}
